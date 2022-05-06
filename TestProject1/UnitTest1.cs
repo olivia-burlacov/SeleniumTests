ec53772a14a47d86cbe71c86b531b2ca215342b6
Olivia Burlacov <olivia.burlacov@gmail.com>
2022-05-06 12:29:51 +0300

    in progress

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,36 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://projectplanappweb-stage.azurewebsites.net/login");
+
+            Thread.Sleep(3000);
+            driver.FindElement(By.CssSelector("div .button > span")).Click();
+
+            Thread.Sleep(3000);
+            driver.FindElement(By.XPath("//input[@type='email']")).SendKeys("automation.pp@amdaris.com");
+
+            Thread.Sleep(3000);
+            driver.FindElement(By.XPath("//input[@type='submit']")).Click();
+
+            Thread.Sleep(3000);
+            driver.FindElement(By.XPath("//input[@type='password']")).SendKeys("10704-observe-MODERN-products-STRAIGHT-69112");
+
+            Thread.Sleep(3000);
+            driver.FindElement(By.XPath("//input[@type='submit']")).Click();
+
+            Thread.Sleep(3000);
+            string actualResult = driver.FindElement(By.Id("//div[@id='displayName']")).Text;
+            Assert.AreEqual()
+        }
+    }
+}
